cec8a0941b142d53adcca033d101a884295e4a8e
user <aaa@wp.pl>
2019-10-18 13:40:26 +0200

    switch

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let red = 100;
 let green = 100;
 let blue = 100;
-
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
-const changeColor = (e) => {
+const changeColor = (e) => {/*
   console.log(e.keyCode, e.which);
 
   if (e.keyCode === 38 && red < 255) {
@@ -20,6 +19,15 @@ const changeColor = (e) => {
     blue--;
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
+  }
+}*/
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
   }
 }
 
